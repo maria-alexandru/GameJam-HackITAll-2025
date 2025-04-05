27882f8456090a9bc49ccf830cfa4f4d0fffa464
Theodor Constantinescu <theokaas14@gmail.com>
2025-04-05 13:24:47 +0300

    miscare
    deplasare + margini

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CameraFollow margins;
+
+    // orientation -> true: right -> false: left
+    private bool orientation;
+    private float move;
+    public float speed;
+    private Rigidbody2D rigidBody;
+    private Transform player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        margins = GameObject.Find("Camera").GetComponent<CameraFollow>();
+        orientation = true;
+        rigidBody = this.GetComponent<Rigidbody2D>();
+        player = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+
+        if(rigidBody != null)
+            rigidBody.linearVelocity = new Vector2(move * speed, 0);
+
+        Orientation();
+
+        // limitez coordonatele
+        if(player != null)
+            player.position = new Vector3(Mathf.Clamp(player.position.x, margins.getMinX(), margins.getMaxX()), player.position.y, player.position.z);
+    }
+
+    void Orientation() {
+        if((orientation && Input.GetKeyDown(KeyCode.D)) == true)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+
+            orientation = false;
+        } 
+        
+        if(((orientation == false) && Input.GetKeyDown(KeyCode.A)) == true) {
+            
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+
+            orientation = true;
+        }
+    }
+}
